541a2a9a767a4115098b195c3c858a2807a3281d
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-09-03 23:53:17 +0300

    ft: send message to my inbox

--- controllers/contact.R
+++ controllers/contact.R
@@ -1,12 +1,21 @@
 box::use(
+  httr2[
+    request,
+    req_perform,
+    req_url_path,
+    last_response,
+    req_body_form,
+    req_auth_basic,
+    resp_body_json,
+  ],
   htmltools[tags, tagList],
   ambiorix[parse_multipart],
   .. / store / mod[
     contact_page = contact,
     text_input,
-    error_alert,
     contact_form,
-    success_alert,
+    toastr_error,
+    toastr_success,
     text_area_input,
     default_contact_form,
   ],
@@ -50,21 +59,52 @@ contact_post <- \(req, res) {
   subject <- validate_subject(body$subject)
   message <- validate_message(body$message)
 
+  validated_form <- contact_form(
+    name = name$validations,
+    email = email$validations,
+    subject = subject$validations,
+    message = message$validations
+  )
+  container <- \(...) {
+    tags$div(
+      id = "form_container",
+      class = "col-lg-7",
+      ...
+    )
+  }
+
   all_ok <- name$ok && email$ok && subject$ok && message$ok
   if (!all_ok) {
-    response <- contact_form(
-      name = name$validations,
-      email = email$validations,
-      subject = subject$validations,
-      message = message$validations
+    return(
+      res$send(
+        container(validated_form)
+      )
+    )
+  }
+
+  sent <- send_message(
+    name = name$value,
+    email = email$value,
+    subject = subject$value,
+    message = message$value
+  )
+
+  if (!sent$ok) {
+    response <- container(
+      toastr_error(
+        msg = "An error occurred while sending your message. Please retry."
+      ),
+      validated_form
     )
     return(
       res$send(response)
     )
   }
 
-  response <- tags$div(
-    success_alert(),
+  response <- container(
+    toastr_success(
+      msg = "Thank you! Your message has been sent. I'll get back to you soon."
+    ),
     default_contact_form()
   )
 
@@ -211,12 +251,55 @@ validate_message <- \(message) {
   list(
     ok = ok,
     value = message,
-    validations = text_input(
+    validations = text_area_input(
       validations,
       label = "Message",
       id = "message",
       value = message,
       class = input_class
     )
+  )
+}
+
+#' Send message via email
+#'
+#' @details This forwards the entered message to my inbox
+#' @param name String. Name of sender.
+#' @param email String. Email of sender.
+#' @param subject String. Email subject.
+#' @param message String. Message.
+#' @return `NULL`
+#' @export
+send_message <- \(
+  name,
+  email,
+  subject,
+  message
+) {
+  message <- sprintf("From: %s\nEmail: %s\n\n%s", name, email, message)
+
+  req <- request(base_url = "https://api.mailgun.net") |>
+    req_url_path("/v3/mg.mwavu.com/messages") |>
+    req_body_form(
+      from = paste(name, "<mailgun@mg.mwavu.com>"),
+      to = "mwavukennedy@gmail.com",
+      subject = subject,
+      text = message
+    ) |>
+    req_auth_basic(username = "api", password = Sys.getenv("MAILGUN_API_KEY"))
+
+
+  tryCatch(
+    expr = {
+      out <- req |>
+        req_perform() |>
+        resp_body_json()
+      out$ok <- TRUE
+      out
+    },
+    error = \(e) {
+      print(conditionMessage(e))
+      list(ok = FALSE)
+    }
   )
 }
